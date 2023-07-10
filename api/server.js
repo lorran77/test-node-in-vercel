cb3923b34438acc6b6be9fb73c339d7265e9d646
lorran azevedo <lorranazevedo153@gmail.com>
2023-07-10 18:02:52 -0300

    teste 6

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const middlewares = jsonServer.defaults()
 server.use(middlewares)
 // Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/src/*': '/$1',
+    '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
